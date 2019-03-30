7d76c30b54a37b80f75265bc2fc75430f412daac
James Thorpe <james.thorpe@utexas.edu>
2019-03-30 16:11:58 -0400

    implemented normalization constants

--- src/ints_HO.f90
+++ src/ints_HO.f90
@@ -15,12 +15,13 @@ CONTAINS
 !               -calculates 1D harmonic oscillator integrals 
 !---------------------------------------------------------------------
 ! Variables
-! N        : int, number of harmonic oscillator basis functions
+! N             : int, number of harmonic oscillator basis functions
 ! Hij		: 2D real*8, Hamiltonian Matrix
 ! Vq            : 1D real*8, 1D potential energy surface
 ! qmin          : real*8, minimum r
 ! qmax          : real*8, max r
 ! qeq           : real*8, equilibriu q
+! Ncon          : 1D real*8, list of normalization constants
 ! error         : bool, true if error
 
 SUBROUTINE HO1D_integrals(N,Vq,q,qmin,qmax,qeq,npoints,k,m,V_off,a,Hij,error)
@@ -31,17 +32,20 @@ SUBROUTINE HO1D_integrals(N,Vq,q,qmin,qmax,qeq,npoints,k,m,V_off,a,Hij,error)
   LOGICAL, INTENT(INOUT) :: error
   INTEGER, INTENT(IN) :: N, npoints
 
+  REAL(KIND=8), DIMENSION(:), ALLOCATABLE :: Ncon
   error = .FALSE.
 
   WRITE(*,*) 
   WRITE(*,*) "Calculating HO integrals"
 
   ALLOCATE(Hij(0:N-1,0:N-1))
+  ALLOCATE(Ncon(0:N-1))
   Hij = 0.0D0
+  Ncon = 0.0D0
 
-  !CALL HO1D_kinetic()
   CALL HO1D_potential(Hij,N,Vq,q,npoints,k,m,V_off,a,error)
-  !CALL HO1D_normalize()
+  CALL HO1D_normalize(Hij,N,a,Ncon,error)
+  !CALL HO1D_kinetic()
 
   !TESTING TESTING TESTING
   WRITE(*,*) "Testing Integrals"
@@ -141,5 +145,53 @@ SUBROUTINE build_Htab(N,q,Htab)
   END IF 
 
 END SUBROUTINE build_Htab
+
+!---------------------------------------------------------------------
+!       HO1D_normalize
+!               -normalizes the matrix elements in Hij
+!               -creates list of normalization constants
+!---------------------------------------------------------------------
+! Variables
+! Hij           : 2D real*8, matrix to be normalized
+! N             : int, number of basis functions
+! a             : real*8, alpha of basis functions 
+! Ncon          : 1D real*8, list of normalization constants
+! error         : bool, true on exit if problem
+
+SUBROUTINE  HO1D_normalize(Hij,N,a,Ncon,error)
+  IMPLICIT NONE
+ 
+  REAL(KIND=8), DIMENSION(0:,0:), INTENT(INOUT) :: Hij
+  REAL(KIND=8), DIMENSION(0:), INTENT(INOUT) :: Ncon
+  REAL(KIND=8), INTENT(IN) :: a
+  LOGICAL, INTENT(INOUT) :: error
+  INTEGER, INTENT(IN) :: N
+
+  REAL(KIND=8) :: pi, const
+  INTEGER :: pwr, fct
+  INTEGER :: i,j
+
+  error = .FALSE.
+  pi = 3.1415926535897932
+  const = a/SQRT(pi)
+
+  Ncon(0) = SQRT(1*const) 
+  
+  pwr = 1
+  fct = 1
+  DO i=1,N-1
+    pwr = pwr * 2
+    fct = i*fct 
+    Ncon(i) = SQRT(1.0/(pwr*fct)*const)
+  END DO
+
+  DO j=0,N-1
+    DO i=j,N-1
+      Hij(i,j) = Hij(i,j)*Ncon(i)*Ncon(j)
+    END DO
+  END DO
+
+END SUBROUTINE HO1D_normalize
+
 !---------------------------------------------------------------------
 END MODULE ints_HO
